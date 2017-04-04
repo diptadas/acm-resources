6231fab2c6a06c6ddf3524ef1ec731fb952b207c
diptadas <dipta670@gmail.com>
2017-04-05 00:00:00 +0600

    Added templates

---
+++ templates/template.cpp
@@ -0,0 +1,40 @@
+/**
+ *    @author     : Dipta Das
+ *    @University : CUET CSE 11
+ *    @Codeforces : diptadas
+ *    @Topcoder   : dipta670
+ *    @link       : www.fb.com/dipta.das
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define LL         long long
+#define pll        pair<LL,LL>
+#define mp         make_pair
+#define fr         first
+#define sc         second
+#define pb         push_back
+
+#define read(n)    scanf("%lld",&n)
+#define mset(a,s)  memset(a,s,sizeof(a))
+#define all(a)     a.begin(),a.end()
+
+#define PI         acos(-1.0)
+#define EPS        1e-9
+#define MOD        1000000007
+#define INF        1e9
+#define NN         1000009
+
+
+int main()
+{
+
+#ifdef input_file
+    //freopen("input.txt","r",stdin);
+#endif
+
+    //ios_base::sync_with_stdio(0),cin.tie(0);
+
+    return 0;
+}
